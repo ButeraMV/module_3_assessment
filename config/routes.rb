d0389d560e3e1b11a0878aecc0f07d6de6fb786c
Boots2291 <buteramv@gmail.com>
2017-11-01 09:29:24 -0600

    Add spec for user searching for stores

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index'
 
+  post '/search', to: 'search#index', as: 'search'
   resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
